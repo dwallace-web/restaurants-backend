03f7b77cd9572489a96b9b37eb462f110d67a891
David Wallace <dwallacemarketing@gmail.com>
2020-12-10 17:42:45 -0500

    add github

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const app = Express();
 const database = require('./db');
 database.sync();
 app.use(Express.json());
+
+
+app.listen(process.env.PORT, () => console.log(`App is listening on the Port number: ${process.env.PORT}`));
