cff93828e1a37b312d2f603112ee6a8ff96b915a
David Wallace <dwallacemarketing@gmail.com>
2020-12-15 21:08:26 -0500

    all comment endpoints - get, get comment by restaurant, edit, put, delete

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,15 +5,34 @@ const commentController = Router();
 //get all comments
 commentController.get('/', async (req, res) => {
     try {
-        await 
-        comment.findAll()
-        .then(comments => 
-            res.status(200).json({
-                message: 'comments Found',
-                data: comments
-            })
-            
-        )
+        await
+        Comment.findAll()
+                .then(comments =>
+                    res.status(200).json({
+                        message: 'Comments Found',
+                        data: comments
+                    })
+
+                )
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
+})
+
+//get comments by restaurant
+commentController.get('/:id', async (req, res) => {
+    try {
+        await
+        Comment.findAll({
+            where: { restaurantId: req.params.id}
+        })
+                .then(comments =>
+                    res.status(200).json({
+                        message: 'Comments Found',
+                        data: comments
+                    })
+
+                )
     } catch (e) {
         res.status(500).json({ error: e })
     }
@@ -22,63 +41,66 @@ commentController.get('/', async (req, res) => {
 commentController.post('/', async (req, res) => {
     try {
         // console.log(req);
-        await 
-        Comment
-        .create(
-            {
-                title: req.body.title,
-                body: req.body.body,
-                userId: req.user.id,
-                restaurantId: req.body.restaurantid
-            }
-        )
-        res.status(200).json({
-            message: 'New comment Created!'
-        })
-        
+        await
+            Comment
+                .create(
+                    {
+                        title: req.body.title,
+                        body: req.body.body,
+                        userId: req.user.id,
+                        restaurantId: req.body.restaurantid
+                    }
+                )
+                .then(updates =>
+                    res.status(200).json({
+                        message: 'New comment Created!',
+                        data: updates
+                    })
+
+                )
     } catch (e) {
-        res.status(500).json({ error: e})
+        res.status(500).json({ error: e })
     }
 })
 
-// commentController.put('/:id', async (req, res) => {
-//     try {
-        
-//         const query = { where: { id: req.params.id, userId: req.user.id } };
-//         const updatedDetails = {
-//             name: req.body.name,
-//             address: req.body.address,
-//             socialmedia: req.body.socialmedia
-//         }
-//         comment
-//         .update(updatedDetails, query)
-//         .then(updates =>
-//             res.status(200).json({
-//                 message: 'Edited comment',
-//                 data: updates
-//             })
-            
-//         )
-//     } catch (e) {
-//         res.status(500).json({ error: e })
-//     }
-// })
+commentController.put('/:id', async (req, res) => {
+    try {
 
-// commentController.delete('/:id', async (req, res) => {
-//     try {
-//         const query = { where: { id: req.params.id, userId: req.user.id } };
+        const query = { where: { id: req.params.id, userId: req.user.id} };
 
-//         comment
-//         .destroy(query)
-//         .then(() => res.status(200).json({
-//             message: 'comment was deleted',
-//         })
-        
-//         )
-//     } catch (e) {
-//         res.status(500).json({ error: e })
-//     }
-// })
+        const updatedComment = {
+            title: req.body.title,
+            body: req.body.body
+        }
+        Comment
+            .update(updatedComment, query)
+            .then(updates =>
+                res.status(200).json({
+                    message: 'Edited comment',
+                    data: updates
+                })
+            )
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
+})
+
+commentController.delete('/:id', async (req, res) => {
+    try {
+        const query = { where: { id: req.params.id, userId: req.user.id } };
+
+        Comment
+            .destroy(query)
+            .then(deleted => res.status(200).json({
+                message: 'comment was deleted',
+                data: deleted
+            })
+
+            )
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
+})
 
 
 
